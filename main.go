b2b5c19ad61a3239de5b48115acc7ce799709d2f
Denis Svechnikov <denis.23weer@gmail.com>
2024-09-17 19:29:39 +0300

    first commit

--- main.go
+++ main.go
@@ -1,38 +1,12 @@
 package main
 
-import "github.com/fishmanDK/cli-indexer/cmd"
+import (
+	"fmt"
+
+	"github.com/fishmanDK/cli-indexer/cmd"
+)
 
 func main() {
+	fmt.Println("start cli-indexer")
 	cmd.Execute()
 }
-
-
-
-// package main
-
-// import (
-// 	"log"
-
-// 	"github.com/fishmanDK/cli-indexer/indexer"
-// 	"github.com/spf13/pflag"
-// 	"github.com/spf13/viper"
-// )
-
-// func main() {
-// 	rpcUrl := pflag.String("rpc", "", "url blockchain rpc")
-// 	startBlock := pflag.Int64("start", 0, "number of block where parsing begins")
-// 	outputFile := pflag.String("out", "", "name of file")
-
-// 	pflag.Parse()
-// 	viper.BindPFlags(pflag.CommandLine)
-
-// 	if *rpcUrl == "" || *startBlock <= 0 || *outputFile == ""{
-// 		log.Fatal("all flags must be specified")
-// 	}
-	
-// 	err := indexer.RunIndexer(*rpcUrl, *startBlock, *outputFile)
-// 	if err != nil{
-// 		log.Fatalf("error indexer: %v", err)
-// 	}
-// }
-
